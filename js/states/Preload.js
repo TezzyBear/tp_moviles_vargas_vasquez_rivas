e9e925a67c3f3cbb7aa2ec0f8baf25475aa3d844
Renzo Vargas <teztortsg@gmail.com>
2020-06-04 22:03:17 -0500

    u

--- js/states/Preload.js
+++ js/states/Preload.js
@@ -6,7 +6,7 @@ Preload.prototype = {
 		this.game.load.image('iconBob', 'assets/dibujos/bobesponja.png');	
 		this.game.load.image('bg', 'assets/dibujos/bg.jpg');	
 		this.game.load.video('bobEsponja', 'assets/videos/spongebob.mp4');
-		this.game.load.video('flamingo', 'assets/videos/flamingo.mp4');
+		this.game.load.video('flamingo', 'assets/videos/Flamingo.mp4');
 	},
 	create:function(){
 		this.state.start("Menu",true,false);
