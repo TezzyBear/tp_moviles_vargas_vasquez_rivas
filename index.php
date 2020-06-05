0be95a40d624b732eeeecfc31b514c2a1a54c08b
Renzo Vargas <teztortsg@gmail.com>
2020-06-04 19:22:33 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		html{
+			padding: 0;
+			margin: 0;
+		}
+	</style>
+</head>
+<body>
+	<script type="text/javascript" src="lib/phaser.min.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/states/Preload.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/states/Game.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/prefabs/Patch.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/prefabs/Button.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/prefabs/Zombie.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/prefabs/Sun.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/prefabs/Plant.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/prefabs/Bullet.js?v=<?php echo time() ?>"></script>
+	<script type="text/javascript" src="js/main.js?v=<?php echo time() ?>"></script>
+</body>
+</html>
