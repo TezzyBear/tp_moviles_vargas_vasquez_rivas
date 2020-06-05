430bfd66af95132a2292a9fab2e667c125a29d01
Renzo Vargas <teztortsg@gmail.com>
2020-06-04 19:29:46 -0500

    Reseteado de template

--- index.php
+++ index.php
@@ -13,12 +13,6 @@
 	<script type="text/javascript" src="lib/phaser.min.js?v=<?php echo time() ?>"></script>
 	<script type="text/javascript" src="js/states/Preload.js?v=<?php echo time() ?>"></script>
 	<script type="text/javascript" src="js/states/Game.js?v=<?php echo time() ?>"></script>
-	<script type="text/javascript" src="js/prefabs/Patch.js?v=<?php echo time() ?>"></script>
-	<script type="text/javascript" src="js/prefabs/Button.js?v=<?php echo time() ?>"></script>
-	<script type="text/javascript" src="js/prefabs/Zombie.js?v=<?php echo time() ?>"></script>
-	<script type="text/javascript" src="js/prefabs/Sun.js?v=<?php echo time() ?>"></script>
-	<script type="text/javascript" src="js/prefabs/Plant.js?v=<?php echo time() ?>"></script>
-	<script type="text/javascript" src="js/prefabs/Bullet.js?v=<?php echo time() ?>"></script>
 	<script type="text/javascript" src="js/main.js?v=<?php echo time() ?>"></script>
 </body>
 </html>
